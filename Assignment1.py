eff6fa7bed16112f1f6f17b30f098b1d0f3eb8c1
deepu3027 <deepu3027@gmail.com>
2016-01-31 17:03:10 -0600

    * Updated Python file and readme

--- Assignment1.py
+++ Assignment1.py
@@ -1,3 +1,10 @@
+#Name: Deepak Thipeswamy
+#Course Number: CSE 6331
+#Lab: Programming Assignment- 1
+
+#Reference:
+# 1. Source code provided in Class 1 and code sheet that was given in class
+
 import glob
 import sys
 import pdb
@@ -9,77 +16,89 @@ from os.path import isfile, join
 
 container_name = 'new-container'
 def start():
-    path = '/home/hadoop/cloud1/uploads/*.*'
-    files = glob.glob(path)
-    for curFile in files: # 'file' is a builtin type, 'name' is a less-ambiguous variable name.
-            with open(curFile) as f: # No need to specify 'r': this is the default.
-                #sys.stdout.write(f.read())
-                file_upld(f)
+	# Get user input
+	option = raw_input('Enter 1 for upload, enter 2 for download: \n')
+	if(int(option)==1):	
+	    path = os.getcwd()+'/uploads/*.*'
+	    files = glob.glob(path)
+	    for curFile in files: 
+	            with open(curFile) as f: 
+	                file_upld(f)
+	else:
+		option = raw_input('Enter the file name with extension: \n')
+		status = download_file(str(option))
+		print status
 
 def file_upld(f):
-    #pdb.set_trace()
-    print 'In file_upld'
-    conn = get_conn()       
-    create_cont(conn)
-    encrypt(f)
-    filename = put_file(conn, f)
-    list_obj(conn)     
-    download_file(filename)
+	print 'In file_upld'
+	oriFilename = f.name.split("/")[-1]
+	conn = get_conn()
+	create_cont(conn)
+    #delete_obj(f.filename, conn)
+	encrypt(f)
+	put_file(conn, oriFilename)
+	list_obj(conn)     
 
-def put_file(conn, f):
-# creates the file if not present and opens in write mode / reads the existing file  
-    print 'In Create File'
-    filename = f.name.split("/")[-1]+'_enc'
-    #pdb.set_trace()
-    with open(filename, 'r') as ex_file:
+def put_file(conn, oriFilename):
+    print 'In Create File'    
+    # Create the file on the container
+    with open(oriFilename, 'r') as ex_file:
             d = ex_file.read()
     conn.put_object(container_name,
-            filename,
+            oriFilename,
             contents= d,
             content_type='text/plain')
     print 'File created successfully'
-    return filename 
 
 def download_file(filename):
-    # Download an object and save it to ./my_file.txt
     print 'In download_file'
+    # Download the file from the container 
     conn = get_conn()
-    obj = conn.get_object(container_name, filename)
-    #pdb.set_trace()
+    try:
+    	obj = conn.get_object(container_name, filename)
+    except:
+    	return 'File not Found'
     fname = filename.split("_")[0]
     save_path = './downloads'
     completeName = os.path.join(save_path, fname)
+    # Write the object to a temporary file
     with open(completeName+'_enc', 'w') as my_file:
             my_file.write(obj[1])
             my_file.close()
     gpg = gnupg.GPG(gnupghome='/usr/lib/gnupg/.gnupg')
+    # Decrypt the file
     with open(completeName+'_enc', 'r') as my_file1:  
             d = my_file1.read()
             data = gpg.decrypt(d, passphrase='xxxxx')
             my_file1.close()
             print "\nObject %s downloaded successfully." % fname
+    # Write the decrypted content to /downloads folder
     with open(completeName+'_decrypted.txt', 'w') as final_file:
     		final_file.write(str(data))
+    		final_file.close()
+		return 'Downloaded successfully'
 
 def encrypt(curfile):
-    filename = curfile.name.split("/")[-1]
+	# Encrypt the file
     print 'In Encrypt file'
-    #pdb.set_trace()
+    filename = curfile.name.split("/")[-1]
+    # Get gnupg object
     gpg = gnupg.GPG(gnupghome='/usr/lib/gnupg/.gnupg')
     input_data = gpg.gen_key_input(key_type="RSA", key_length=128, passphrase='xxxxx')
+    # Generate key
     key = gpg.gen_key(input_data)
-    status = gpg.encrypt_file(curfile, str(key), always_trust=True, output=filename+'_enc')
+    # Encrypt
+    status = gpg.encrypt_file(curfile, str(key), always_trust=True, output=filename)
     print status.ok
                 
 def get_conn():
     # swift client connection object
     auth_url = 'https://identity.open.softlayer.com/v3'
-    # OpenStack project unique id for IBM Bluemix authentication service
     project_id = '25085d2bdb6c4b0ea73424af654f5623'
     user_id = 'f92543b6fb66460e8c1a72c1a9bca0f6'
     region_name = 'dallas'
     password = 'B*!F*?3LT37f{,Im'
-    #pdb.set_trace()
+    # Get connection object
     print 'In get_conn'
     conn = swiftclient.Connection(
             key=password,
@@ -93,6 +112,7 @@ def get_conn():
 
 def create_cont(conn):
     print 'In create_cont'
+    # Create a container
     conn.put_container(container_name)
     print "\nContainer %s created successfully." % container_name
         
@@ -104,22 +124,21 @@ def list_cont(conn):
         print c['name']
 
 def list_obj(conn):     
-    # List objects in a container, and prints out each object name, the file size, and last modified date
     print ("\nObject List:")
+    # List all objects in the container
     for container in conn.get_account()[1]:
         for data in conn.get_container(container['name'])[1]:
             print 'object: {0}\t size: {1}\t date: {2}'.format(data['name'], data['bytes'], data['last_modified'])
 
 def delete_obj(filename, conn):
-    # Delete an object
+    # Delete an object from the container
     conn.delete_object(container_name, filename)
     print "\nObject %s deleted successfully." % filename
 
 def delete_cont(conn):  
-    # To delete a container. Note: The container must be empty!
+	# Delete the container
     conn.delete_container(container_name)
     print "\nContainer %s deleted successfully.\n" % container_name
-
 
 if __name__ == '__main__':
     start()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# IBMBluemixStorageAssignment
-IBMBluemixStorageAssignment
+Use: python Assignment1.py
+
+* Drop your files to encrypt and upload in the '/uploads' folder
+* All decrypted files will be downloaded to '/downloads' folder'
